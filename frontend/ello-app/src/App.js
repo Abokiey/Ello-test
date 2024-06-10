6dfed8f1fb8ee10917706337fae94adef7f77733
Abokiey <sweneysam@gmail.com>
2024-06-10 16:45:56 +0300

    removal errors from the frontend directory and cleaning of data

---
+++ frontend/ello-app/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+      the book assignment view
+      </p>
+    </div>
+  );
+}
+
+export default App;
